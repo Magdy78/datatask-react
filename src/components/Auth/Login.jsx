3800de551473f473806699d7394b3d54051f6262
Magdy78 <azzam9778@gmail.com>
2023-08-24 00:36:16 +0300

    Final

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -33,6 +33,7 @@ const Login = () => {
   return (
     <div className="auth-container">
       <h2>Login</h2>
+      <br />
       <form onSubmit={handleLogin}>
         <input
           type="email"
@@ -41,6 +42,7 @@ const Login = () => {
           onChange={e => setEmail(e.target.value)}
           required
         />
+        <br />
         <input
           type="password"
           placeholder="Password"
@@ -48,6 +50,7 @@ const Login = () => {
           onChange={e => setPassword(e.target.value)}
           required
         />
+        <br />
         <button type="submit">Login</button>
       </form>
       {error && <p className="error">{error}</p>}
--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'; 
-import axiosInstance from './services/axiosInstance';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axiosInstance from "./services/axiosInstance";
 import "./ItemDetails.css";
 const ItemDetails = () => {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const [itemDetails, setItemDetails] = useState(null);
 
   useEffect(() => {
-    axiosInstance.get(`/products/${id}`)
-      .then(data => {
-        console.log('Item details:', data);
-        setItemDetails(data); 
+    axiosInstance
+      .get(`/products/${id}`)
+      .then((data) => {
+        console.log("Item details:", data);
+        setItemDetails(data);
       })
-      .catch(error => console.error('Error fetching item details:', error));
+      .catch((error) => console.error("Error fetching item details:", error));
   }, [id]);
 
   if (!itemDetails) {
@@ -22,12 +23,11 @@ const ItemDetails = () => {
   return (
     <div className="item-details">
       <h2>{itemDetails.title}</h2>
-      
-      <img src={itemDetails.thumbnail}/>
+
+      <img src={itemDetails.thumbnail} />
       <p>{itemDetails.description}</p>
       <button className="btn btn-primary">Add to Cart</button>
       <button className="btn btn-secondary">Add to Wishlist</button>
-    
     </div>
   );
 };
